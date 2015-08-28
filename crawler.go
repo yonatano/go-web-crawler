b9acc8ad1aac3929d6baaf4c06e319411464cc78
Yonatan Oren <yonatano@gmail.com>
2015-08-27 17:27:46 -0700

    crawls. Need to implement buckets, workerpool

--- crawler.go
+++ crawler.go
@@ -5,28 +5,52 @@ import (
 	// "regexp"
 	// "io"
 	"bytes"
+	"github.com/PuerkitoBio/purell"
+	"github.com/oleiade/lane"
 	"gopkg.in/xmlpath.v2"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 )
 
 type Crawler struct {
-	queue []string
+	queue *lane.Queue
 }
-
-// func (c *Crawler)Enqueue
 
 type Page struct {
 	url    string
 	source []byte
 }
 
+func (c *Crawler) Crawl() {
+	var linkQueue *lane.Queue = c.queue
+	fmt.Println(linkQueue)
+	for {
+		link := linkQueue.Dequeue()
+		if linkstr, ok := link.(string); ok {
+			//fetch data with link
+			p := Page{url: linkstr}
+			p.Fetch()
+			fetched := p.GetLinks()
+			//add new links to queue
+			for _, l := range fetched {
+				linkQueue.Enqueue(l)
+			}
+			//report
+			fmt.Println("crawled: %s added %i new links", linkstr, len(fetched))
+		} else {
+			break
+		}
+	}
+}
+
 func (p *Page) Fetch() {
 	//make request
 	resp, err := http.Get(p.url)
 	if err != nil {
-		// handle error
-		fmt.Println("error: ", err)
+		// fail and requeue for later
+		panic(err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
@@ -46,20 +70,33 @@ func (p *Page) GetLinks() []string {
 
 	var links []string
 	for iter.Next() {
-		url := iter.Node().String()
-		links = append(links, url)
+		link := iter.Node().String()
+		//fix and normalize url
+		link = FixUrl(link, p.url)
+		links = append(links, link)
 	}
 	return links
 }
 
-// func (p *page)
+func FixUrl(link string, parent string) string {
+	//normalize url
+	normalized := purell.MustNormalizeURLString(link, purell.FlagsSafe)
+	u, err := url.Parse(normalized)
+	if err != nil {
+		log.Fatal(err)
+	}
+	base, err := url.Parse(parent)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return base.ResolveReference(u).String()
+}
 
 func main() {
-	p := Page{url: "http://wikipedia.org/"}
-	p.Fetch()
-	links := p.GetLinks()
-	fmt.Println(links)
-	//fmt.Println("source:\n", p.source)
+	var seed *lane.Queue = lane.NewQueue()
+	seed.Enqueue("http://wikipedia.com/")
+	var crawler = Crawler{queue: seed}
+	crawler.Crawl()
 	// re := regexp.MustCompile("href=""")
 	// fmt.Println(re.MatchString("paranormal"))
 }
